f57ac6c8ef5a5cba44eec00bb7e4eec9c91175ed
myerscody16 <myerscody16@live.com>
2019-08-26 07:58:58 -0400

    initial

---
+++ Capstone4/Controllers/TaskListController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone4.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone4.Controllers
+{
+    public class TaskListController : Controller
+    {
+        private readonly TaskListCapstone4DbContext _context;
+
+        public TaskListController(TaskListCapstone4DbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult AddTask(Tasks newTask)
+        {
+            AspNetUsers thisUser = _context.AspNetUsers.Where(x => x.UserName == User.Identity.Name).First();
+            newTask.UserId = thisUser.Id;
+            _context.Tasks.Add(newTask);
+            _context.SaveChanges();
+            List<Tasks> listOfTasks = _context.Tasks.ToList();
+            ViewBag.listOfTasks = listOfTasks;
+            return RedirectToAction("ViewTaskList");
+        }
+        public IActionResult ViewTaskList()
+        {
+            return View();
+        }
+    }
+}
---
+++ Capstone4/Models/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Capstone4.Models
+{
+    public partial class Tasks
+    {
+        public int TaskId { get; set; }
+        public string UserId { get; set; }
+        public string TaskDesc { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string CompleteStatus { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
